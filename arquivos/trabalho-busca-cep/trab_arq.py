84f7d9ba922586f08da1355f32f38607ac186d1f
Cássio Fernando <61878987+cassiofb-dev@users.noreply.github.com>
2020-03-18 01:21:08 -0300

    Add files via upload

---
+++ arquivos/trabalho-busca-cep/trab_arq.py
@@ -0,0 +1,45 @@
+import sys
+import struct
+
+def to_bit(n):
+    return n*300
+
+if len(sys.argv) != 2 and len(sys.argv[1]) != 8:
+	print ("USO %s [CEP]" % sys.argv[0])
+	quit()
+
+registroCEP = struct.Struct("72s72s72s72s2s8s2s")
+cepColumn = 5
+count = 0
+
+with open("cep_ordenado.dat","rb") as f:
+    ini = 0
+    fim = int(f.seek(0,2)/300)
+
+    while ini <= fim:
+        count += 1
+        mid = int((ini+fim)/2)
+
+        f.seek(to_bit(mid),0)
+
+        linha = f.read(registroCEP.size)
+        endereco = registroCEP.unpack(f.read(registroCEP.size))
+        cep = str(endereco[cepColumn],'latin1')
+
+        if cep == sys.argv[1]:
+            for e in endereco:
+                print(str(e,'latin1'))
+            break
+
+        elif cep < sys.argv[1]:
+            ini = mid + 1
+            pass
+
+        else:
+            fim = mid - 1
+            pass
+
+        pass
+    pass
+
+print("Busca completa!\nTotal lido:",count)
